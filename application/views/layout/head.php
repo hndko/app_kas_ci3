a7ae94b12c32e19180900c9c557b97211c1aa15e
hndko <hd.doko22@gmail.com>
2026-01-02 11:27:36 +0700

    chore: Remove unused image assets and consolidate favicon/apple-touch-icon to logo.png.

--- application/views/layout/head.php
+++ application/views/layout/head.php
@@ -10,8 +10,8 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="<?= base_url() ?>assets/img/favicon.png" rel="icon">
-  <link href="<?= base_url() ?>assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="<?= base_url() ?>assets/img/logo.png" rel="icon" type="image/png">
+  <link href="<?= base_url() ?>assets/img/logo.png" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
